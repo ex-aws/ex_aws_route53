a9dabcde503609275e50a9a6e70f318551d74478
Boris Mikhaylov <kaguxmail@gmail.com>
2017-11-01 08:52:30 -0500

    bumped version to 2.0.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExAws.Route53.Mixfile do
   def project do
     [
       app: :ex_aws_route53,
-      version: "2.0.0",
+      version: "2.0.1",
       elixir: "~> 1.5",
       elixirc_paths: elixirc_paths(Mix.env),
       start_permanent: Mix.env == :prod,
