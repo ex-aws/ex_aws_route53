1209ef39f5742a6c85b563c96e801ff8401639d4
Boris Mikhaylov <kaguxmail@gmail.com>
2017-12-20 15:52:13 +0100

    added package metadata

--- mix.exs
+++ mix.exs
@@ -1,14 +1,23 @@
 defmodule ExAws.Route53.Mixfile do
   use Mix.Project
 
+  @version "2.0.2"
+  @service "route53"
+  @url "https://github.com/ex-aws/ex_aws_#{@service}"
+  @name __MODULE__ |> Module.split |> Enum.take(2) |> Enum.join(".")
+
   def project do
     [
       app: :ex_aws_route53,
-      version: "2.0.2",
+      version: @version,
       elixir: "~> 1.5",
       elixirc_paths: elixirc_paths(Mix.env),
       start_permanent: Mix.env == :prod,
-      deps: deps()
+      deps: deps(),
+      name: @name,
+      package: package(),
+      docs: [main: @name, source_ref: "v#{@version}",
+        source_url: @url]
     ]
   end
 
@@ -38,5 +47,14 @@ defmodule ExAws.Route53.Mixfile do
       "LOCAL" -> {:ex_aws, path: "../ex_aws"}
       _ -> {:ex_aws, "~> 2.0.0"}
     end
+  end
+
+  defp package do
+    [description: "#{@name} service package",
+     files: ["lib", "config", "mix.exs", "README*"],
+     maintainers: ["Ben Wilson", "Boris Mikhaylov"],
+     licenses: ["MIT"],
+     links: %{github: @url},
+    ]
   end
 end
