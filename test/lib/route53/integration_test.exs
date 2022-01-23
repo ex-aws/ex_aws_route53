2402d77d97cc97fbe823186576c21839b47f9750
Bernard Duggan <bernard@circl.es>
2022-01-24 10:32:32 +1100

    Skip test requiring AWS

--- test/lib/route53/integration_test.exs
+++ test/lib/route53/integration_test.exs
@@ -1,6 +1,8 @@
 defmodule ExAws.Route53IntergrationTest do
   use ExUnit.Case, async: true
 
+  # Requires access to AWS
+  @tag :skip
   test "#list_hosted_zones" do
     assert {:ok, %{body: body}} = ExAws.Route53.list_hosted_zones(max_items: 5) |> ExAws.request()
     assert body |> Map.has_key?(:hosted_zones)
